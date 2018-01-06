528de86dff781040d82a308f5ec3bb821c60c52e
Robert Moon <roberttmoon@gmail.com>
2018-01-05 23:44:20 -0600

    added in the start of the class to repersent cells

---
+++ src/cell.py
@@ -0,0 +1,18 @@
+class cell:
+    """
+    Define a cell as somethign that can only be one byte.
+    """
+
+    def __init__(self):
+        value = None
+
+    def cell_value(self):
+        """
+        gets cell value
+        """
+
+    def act_on_opperator(self, opperator):
+        """
+        take in cell opperator and exec it against cell
+        valid opperators are + - . ,
+        """
