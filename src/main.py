d06a84a5a201ded4dbfd4a918648e988293b141c
Robert Moon <roberttmoon@gmail.com>
2018-01-05 22:56:15 -0600

    roughing out

---
+++ src/main.py
@@ -0,0 +1,67 @@
+def forward_index():
+    """
+    Called when the char '>' is found
+    moves forward one index in the cell list
+    """
+    pass
+
+
+def back_index():
+    """
+    Called when char '<' is found
+    moves back one index in the cell list
+    """
+    pass
+
+
+def plus_cell():
+    """
+    Called when the '+' char is found
+    adds one bit to the byte in the cell
+    """
+    pass
+
+
+def minus_cell():
+    """
+    Called when the '-' char is found
+    minus one bit from the byte in the cell
+    """
+    pass
+
+
+def print_cell():
+    """
+    Called when the '.' char is found
+    outputs the value of the celll
+    """
+    pass
+
+
+def input_cell():
+    """
+    called when the ',' char is found
+    accept one byte of user input to the cell
+    """
+    pass
+
+
+def loop():
+    """
+    deals with the loop in bf
+    """
+
+    def scan_for_close():
+        """
+        scanns ahead to find the closing loop
+        """
+
+    def non_zero_cell():
+        """
+        deals with a cell that isnt zero
+        """
+
+    def zero_cell():
+        """
+        deals with a cell that is value zero
+        """
